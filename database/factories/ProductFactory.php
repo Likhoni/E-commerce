74157f4441a5b085227c1bb5215bf75fa6c4a9ab
Mahmud Hasan <mahmud00hasan09@gmail.com>
2024-11-05 18:39:17 +0600

    yajra data-table

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_name'=>$this->faker->name(),
+            'category_id'=>$this->faker->numberBetween(1, 4),
+            'product_quantity'=>$this->faker->randomDigit(100, 200),
+            'product_price'=>$this->faker->randomDigit(100, 1000),
+            'discount'=>$this->faker->randomDigit(10, 40),
+            'product_description'=>$this->faker->paragraph(1)
+
+        ];
+    }
+}
--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<style type="text/css"> .notify{ z-index: 1000000; margin-top: 4%; } </style>
+<style type="text/css">
+    .notify {
+        z-index: 1000000;
+        margin-top: 4%;
+    }
+</style>
 @notifyCss
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -11,6 +17,9 @@
     <title>E-Commerce</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="{{url('backend/css/styles.css')}}" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 
@@ -37,6 +46,7 @@
             @include('backend.partial.footer')
         </div>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="{{url('backend/js/scripts.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
@@ -44,9 +54,16 @@
     <script src="{{url('backend/assets/demo/chart-bar-demo.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
     <script src="{{url('backend/js/datatables-simple-demo.js')}}"></script>
-    
+
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
     @include('notify::components.notify')
     @notifyJs
 
+
+    @stack('js')
+
 </body>
+
 </html>
